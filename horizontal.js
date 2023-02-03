2294a9cc6e2638382004d84b10907f332f651064
AminaBrown <amina@datacult.com>
2023-02-03 09:50:14 -0500

    update layout and rect size

--- horizontal.js
+++ horizontal.js
@@ -1,6 +1,6 @@
 'use strict'
 
-let force = ((state = 'koko',selector = '#force-placeholder') => {
+let force = ((state = 'not koko',selector = '#force-placeholder') => {
 
     var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ? true : false
 
@@ -26,7 +26,7 @@ let force = ((state = 'koko',selector = '#force-placeholder') => {
 
     // responsive width & height
     const svgWidth = 1440 
-    const svgHeight = 900 
+    const svgHeight = 850//900 
 
     // helper calculated variables for inner width & height
     const height = svgHeight - margin.top - margin.bottom
@@ -56,15 +56,15 @@ let force = ((state = 'koko',selector = '#force-placeholder') => {
     var caught = Math.round(num_boxes*(value/100))
 
     for (let i = 0; i < caught; i++) {
-        node_data.push({ "id": i+1, "group": 1, "fill": '#ffffff', "r": 30})
+        node_data.push({ "id": i+1, "group": 1, "fill": '#ffffff', "r": 20})
       }
 
     for (let i = 0; i < (num_boxes-caught); i++) {
-        node_data.push({ "id": caught+1+i, "group": 1, "fill": '#c5c5c5', "r": 30})
+        node_data.push({ "id": caught+1+i, "group": 1, "fill": '#c5c5c5', "r": 20})
       }
 
-    var placeholder = 2
-    node_data.splice(placeholder, 0, { "id": num_boxes+1, "group": 1, "fill": '#ffffff', "r": 225});
+    // var placeholder = 2
+    // node_data.splice(placeholder, 0, { "id": num_boxes+1, "group": 1, "fill": '#ffffff', "r": 225});
 
 
     const data = {
@@ -75,6 +75,7 @@ let force = ((state = 'koko',selector = '#force-placeholder') => {
 
     var grid_layout = []
     for (let i = 0; i < num_boxes; i++) {
+    // for (let i = 0; i < num_boxes+1; i++) {
         grid_layout.push({ "id": i+1, "x": 0, "y": 0, "sim_x": 0, "sim_y": 0})
       }
 
@@ -108,19 +109,40 @@ let force = ((state = 'koko',selector = '#force-placeholder') => {
             .on("end", dragended)
     }
 
-
+    var center_y =  height / 2
     var simulation = d3.forceSimulation(nodes)
         .force("charge", d3.forceManyBody().strength(1))
+        .force("radial", d3.forceRadial(d => Math.floor(Math.random() * 270) + 240, width / 2, center_y))
         .force("collide", d3.forceCollide().radius(d => d.r))
-        .force("center", d3.forceCenter(width / 2, height / 2))
+        .force("center", d3.forceCenter(width / 2, center_y))
+        .force("bouding-circle", () => {
+            nodes.forEach(node => {
+              if (node.y < center_y - 350 || node.y > center_y + 340) {
+                // node.x = Math.floor(Math.random() * 270) + 240//... // new x position
+                node.y = Math.floor(Math.random() * (center_y+340)) + (center_y-350)//... // new y position
+              }
+            })
+          })
+        .tick(0)
 
 
 
     ////////////////////////////////////
     //////////// add to DOM ////////////
     ////////////////////////////////////  
+    var text_height = 300, text_width = 400, text_x = width/2-text_width/2, text_y = height/2-text_height/2
+    
+    svg.append('rect')
+    .attr('class','force-text')
+    .attr('id','force-text-koko')
+    .attr('height',text_height)
+    .attr('width',text_width)
+    .attr('x',text_x)
+    .attr('y',text_y)
+    .style('fill','lightgrey')
+    .style('opacity',.2)
 
-        var rect_height = 20, rect_width = 50;
+    var rect_height = 25, rect_width = 75, rect_width_tree = 70, rect_height_tree = 50;
     const node = svg.append("g")
         .attr("stroke", "#fff")
         .attr("stroke-width", 1.5)
@@ -135,21 +157,14 @@ let force = ((state = 'koko',selector = '#force-placeholder') => {
         .style('stroke','black')
         .call(drag(simulation));
 
-    var rect_select = num_boxes+1
-    d3.select('#word'+rect_select)  
-    .style("fill",'none')
-    .style('stroke','none')
+    // var rect_select = num_boxes+1
+    // d3.select('#word'+rect_select)  
+    // .style("fill",'none')
+    // .style('stroke','none')
 
-    var text_height = 300, text_width = 400, text_x = width/2-text_width/2, text_y = height/2-text_height/2-100
-    svg.append('rect')
-        .attr('class','force-text')
-        .attr('id','force-text-koko')
-        .attr('height',text_height)
-        .attr('width',text_width)
-        .attr('x',text_x)
-        .attr('y',text_y)
-        .style('fill','lightgrey')
+    
 
+    // node_data.splice(placeholder,1)
 
     simulation.on("tick", () => {
 
@@ -158,12 +173,16 @@ let force = ((state = 'koko',selector = '#force-placeholder') => {
             .attr("y", d => d.y)
         
     })
-    // .on('end', () => {
-    //   d3.selectAll('circle').each(function () {
-    //     const thisD3 = d3.select(this)
-    //     console.log(thisD3.attr('cx'), thisD3.attr('cy'))
-    //   })
-    // }) 
+    .on('end', () => {
+      d3.selectAll('.forceword').each(function () {
+        const thisD3 = d3.select(this)
+        console.log(thisD3.attr('id').split('d')[1])
+        
+        grid_layout[thisD3.attr('id').split('d')[1]-1].sim_x = thisD3.attr('x')
+        grid_layout[thisD3.attr('id').split('d')[1]-1].sim_y = thisD3.attr('y')
+        // console.log(thisD3.attr('cx'), thisD3.attr('cy'))
+      })
+    }) 
 
 
 
@@ -171,21 +190,28 @@ let force = ((state = 'koko',selector = '#force-placeholder') => {
 function calcGrid(value) {
     var columns = 10, rows = num_boxes/columns;
 
-    var start_x = width/2-rect_width*columns/2, start_y = height/2-rect_height*rows/2;
+    var start_x = width/2-rect_width_tree*columns/2, start_y = height/2-rect_height_tree*rows/2;
 
-    grid_layout[value-1].x = start_x+rect_width*(value-(Math.ceil(value/columns)-1)*columns)-rect_width
-    grid_layout[value-1].y = start_y+rect_height*Math.ceil(value/columns)-rect_height
+    grid_layout[value-1].x = start_x+rect_width_tree*(value-(Math.ceil(value/columns)-1)*columns)-rect_width_tree
+    grid_layout[value-1].y = start_y+rect_height_tree*Math.ceil(value/columns)-rect_height_tree
 
     // grid_layout.push({'id':value,'x':grid_x,'y':grid_y})
 
 }
 
 
-node_data.splice(placeholder,1)
 
 var position = d3.scaleLinear()
     .domain([0,1])
     .range([text_x,-1.1*text_width])
+
+var size_width = d3.scaleLinear()
+    .domain([0,1])
+    .range([rect_width,rect_width_tree])
+
+var size_height = d3.scaleLinear()
+    .domain([0,1])
+    .range([rect_height,rect_height_tree])
 
 function updatePosition(percent) {
    d3.selectAll('.force-text')
@@ -206,6 +232,8 @@ function updatePosition(percent) {
     d3.select('#word'+d.id)
     .attr('x',grid_position_x(percent))
     .attr('y',grid_position_y(percent))
+    .attr('width',size_width(percent))
+    .attr('height',size_height(percent))
    })
 }
 
