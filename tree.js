15b7d506a7ae7c9fd15a3ca46e3f5f7dbd6b4615
AminaBrown <amina@datacult.com>
2023-02-17 14:10:44 -0700

    finish tree function and clean out test files

--- tree.js
+++ tree.js
@@ -1,35 +1,22 @@
-// Copyright 2021 Observable, Inc.
-// Released under the ISC license.
-// https://observablehq.com/@d3/tree
-function Tree(data, { // data is either tabular (array of objects) or hierarchy (nested objects)
-    path, // as an alternative to id and parentId, returns an array identifier, imputing internal nodes
-    id = Array.isArray(data) ? d => d.id : null, // if tabular data, given a d in data, returns a unique identifier (string)
-    parentId = Array.isArray(data) ? d => d.parentId : null, // if tabular data, given a node d, returns its parent’s identifier
-    children, // if hierarchical data, given a d in data, returns its children
-    tree = d3.tree, // layout algorithm (typically d3.tree or d3.cluster)
-    sort, // how to sort nodes prior to layout (e.g., (a, b) => d3.descending(a.height, b.height))
-    label, // given a node d, returns the display name
-    title, // given a node d, returns its hover text
-    link, // given a node d, its link (if any)
-    linkTarget = "_blank", // the target attribute for links (if any)
-    width = 640, // outer width, in pixels
-    height, // outer height, in pixels
-    r = 40, // radius of nodes
-    padding = 1, // horizontal padding for first and last column
-    fill = "#999", // fill for nodes
-    fillOpacity, // fill opacity for nodes
-    stroke = "#B5B5B5", // stroke for links
-    strokeWidth = 2, // stroke width for links
-    strokeOpacity = 1, // stroke opacity for links
-    strokeLinejoin, // stroke line join for links
-    strokeLinecap, // stroke line cap for links
-    halo = "#fff", // color of label halo 
-    haloWidth = 3, // padding around the labels
-    curve = d3.curveBumpX, // curve for the link
-    intervention_type
-  } = {}) {
+'use strict'
 
-//   console.log(intervention_type)
+let tree = ((data, data_map = {width: 640, intervention_type: 'koko'}) => {
+
+    var tree = d3.tree, 
+        id = Array.isArray(data) ? d => d.id : null, 
+        parentId = Array.isArray(data) ? d => d.parentId : null,
+        r = 40,
+        padding = 1,
+        stroke = "#B5B5B5", // stroke for links
+        strokeWidth = 2, // stroke width for links
+        strokeOpacity = 1, // stroke opacity for links
+        strokeLinejoin, // stroke line join for links
+        strokeLinecap, // stroke line cap for links
+        curve = d3.curveBumpX,
+        path, children, sort, height,
+        width = data_map.width,
+        intervention_type = data_map.intervention_type
+
 
     // If id and parentId options are specified, or the path option, use d3.stratify
     // to convert tabular data to a hierarchy; otherwise we assume that the data is
@@ -42,12 +29,9 @@ function Tree(data, { // data is either tabular (array of objects) or hierarchy 
     // Sort the nodes.
     if (sort != null) root.sort(sort);
   
-    // Compute labels and titles.
-    const descendants = root.descendants();
-    const L = label == null ? null : descendants.map(d => label(d.data, d));
   
     // Compute the layout.
-    console.log(root)
+    // console.log(root)
     const dx = 60;
     const dy = 175//width / (root.height + padding);
     tree().nodeSize([dx, dy])(root);
@@ -90,6 +74,8 @@ function Tree(data, { // data is either tabular (array of objects) or hierarchy 
       .selectAll("path")
         .data(lnk)
         .join("path")
+        .attr('class',d => 'link_group'+d.target.data.value)
+        .attr('id',d => d.target.data.name == '. . .' || d.target.data.name == '' ? "empty"+d.source.data.name.replace(/\s/g, '')+'_path' : d.target.data.name.replace(/\s/g, '')+'_path')
           .attr("d", d3.linkHorizontal(curve)
             //   .x(d => {
             //     return d.depth == 2 ? d.y/2 : d.y
@@ -97,39 +83,24 @@ function Tree(data, { // data is either tabular (array of objects) or hierarchy 
               .x(d => d.y)
               .y(d => d.x));
 
-    // console.log(lnk)
-  
-    // const node = svg.append("g")
-    //   .selectAll("a")
-    //   .data(root.descendants())
-    //   .join("a")
-    //     .attr("xlink:href", link == null ? null : d => link(d.data, d))
-    //     .attr("target", link == null ? null : linkTarget)
-    //     .attr("transform", d => `translate(${d.y},${d.x})`);
-  
-    // node.append("rect")
-    //     .attr("fill", d => d.children ? stroke : fill)
-    //         .attr("height", r)
-    //         .attr('width',r);
-
+// console.log(root.descendants())
     const node = svg.append("g")
       .selectAll(".node-group")
       .data(root.descendants())
       .join("g")
-      .attr('class','node-group')
-      .attr('id',d => d.data.name)
-        .attr("transform", d => `translate(${d.y-r*0.5},${d.x})`);
+      .attr('class',d => 'node-group'+d.data.value)
+      .attr('id',d => d.data.name == '. . .' || d.data.name == '' ? "empty"+d.parent.data.name.replace(/\s/g, '') : d.data.name.replace(/\s/g, ''))
+      .attr('opacity',1)
+      .attr("transform", d => `translate(${d.y-r*0.5},${d.x})`);
 
     node.append('rect')
     .attr("opacity", d => d.depth == 2 ? 0 : 1)
     .attr('fill','#22194D')
-    // .attr('opacity',.3)
     .attr("height", r*1.25)
     .attr('width',r*5.25)
     .attr('transform','translate('+r/-7.5+','+r*-.625+')')
 
     node.append('rect')
-    // .attr("fill", d => d.children ? stroke : fill)
     .attr('fill','white')
     .attr("opacity", d => d.depth == 2 ? 0 : .3)
     .attr("height", r)
@@ -138,35 +109,69 @@ function Tree(data, { // data is either tabular (array of objects) or hierarchy 
     .attr('transform','translate(0,'+r/-2+')')
 
     d3.select('#detection').attr('opacity',0)
-  
-    if (title != null) node.append("title")
-        .text(d => title(d.data, d));
-  
-    if (L) node.append("text")
+
+    node.append("text")
         .attr("dy", "0.32em")
-        // .attr("x", d => d.children ? -6 : 6)
         .attr('x',r*5/2)
-        // .attr("text-anchor", d => d.children ? "end" : "start")
         .attr("text-anchor",'middle')
         .attr('fill','white')
         .attr('font-size',16)
-        // .attr("paint-order", "stroke")
-        // .attr("stroke", halo)
-        // .attr("stroke-width", haloWidth)
-        .text((d, i) => L[i]);
+        .text(d => d.data.name);
   
-    return svg.node();
+    // return svg.node();
+// console.log(root.descendants())
 
-     
-  }
+var view = 0, og_position_call, og_position_text, koko_positions = [];
+if (view == 0){
+    og_position_call = document.getElementById('call').getAttribute('transform')
+    og_position_text = document.getElementById('text').getAttribute('transform')
 
+    root.descendants().forEach(el => {
+        // console.log(el.data.name)
+        if (el.data.name == "" || el.data.name == ". . ."){
+            koko_positions.push({"name":"empty"+el.parent.data.name.replace(/\s/g, ''),
+                                "node":document.getElementById("empty"+el.parent.data.name.replace(/\s/g, '')).getAttribute('transform'),
+                                "path":document.getElementById("empty"+el.parent.data.name.replace(/\s/g, '')+"_path").getAttribute('d')
+                            })
+        } else if (el.data.name !== 'call' & el.data.name !== 'text'& el.data.name !== 'detection'){
+            // console.log(el.data.name)
+            koko_positions.push({"name":el.data.name.replace(/\s/g, ''),
+                                "node":document.getElementById(el.data.name.replace(/\s/g, '')).getAttribute('transform'),
+                                "path":document.getElementById(el.data.name.replace(/\s/g, '')+"_path").getAttribute('d')
+                            })
+        }
+    });
+
+}
+console.log(koko_positions)
   function update(val) {
     if (val == 'koko'){
-        // d3.select('#contact').attr('transform','translate(100,20)')
+        d3.select('#call').attr('transform',og_position_call)
+        d3.select('#call_path').attr('d','M350,-255C437.5,-255,437.5,-315,525,-315')
+        d3.select('#text').attr('transform',og_position_text)
+        d3.select('#text_path').attr('d','M350,-255C437.5,-255,437.5,-255,525,-255')
+        d3.selectAll('.node-group0').attr('opacity',1)
+        d3.selectAll('.link_group0').attr('opacity',1)
+        koko_positions.forEach(el => {
+            console.log('move: '+el.name+el.node+el.path),
+            d3.select('#'+el.name).attr('transform',el.node)
+            d3.select('#'+el.name+'_path').attr('d',el.path)
+             
+        })
         // console.log()
     } else {
-        // d3.select('#contact').attr('transform','translate(300,-100)')
+        d3.select('#call').attr('transform','translate(250,-75)')
+        d3.select('#call_path').attr('d','M0,0C125,0,125,-75,250,-75')
+        d3.select('#text').attr('transform','translate(250,75)')
+        d3.select('#text_path').attr('d','M0,0C125,0,125,75,250,75')
+        d3.selectAll('.node-group0').attr('opacity',0).attr('transform','translate(0,0)')
+        d3.selectAll('.link_group0').attr('opacity',0).attr('d','M0,0C0,0,0,0,0,0')
         // document.getElementById('contact').getAttribute('transform')
-        console.log(document.getElementById('contact').getAttribute('transform'))
     }
-    }
+  }
+
+return {
+    update: update,
+}
+
+})
